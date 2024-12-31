143cf2b571aca3aeebdda4ec4131eb44e5af15e9
GabrielEdogawa <bistoury007@gmail.com>
2024-12-31 16:10:57 +0000

    Deploying to gh-pages from @ GabrielEdogawa/gabrieledogawa.github.io@d4808e8801b83150532c10b773354a533c6e8324 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "$$\mathbf{\textit{\Large{vita brevis, ars longa}}}$$",
+          description: "$${\Large \textbf{\textit{vita brevis, ars longa}}}$$",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
